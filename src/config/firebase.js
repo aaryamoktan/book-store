dc8f9c42100925d8fdf0b2f472ae351cb1995250
aarya moktan <aaryamoktan2055@gmail.com>
2023-10-30 18:34:45 -0700

    commit first

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getAuth} from "firebase/auth"
+const firebaseConfig = {
+  apiKey: "AIzaSyAinOAFLOHf0iTz_7KTXNOC7ADRqsjX8to",
+  authDomain: "book-store-8d7a0.firebaseapp.com",
+  projectId: "book-store-8d7a0",
+  storageBucket: "book-store-8d7a0.appspot.com",
+  messagingSenderId: "133777360425",
+  appId: "1:133777360425:web:95510631725d12f7b23e38",
+  measurementId: "G-B59KRQPLZL"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app)
